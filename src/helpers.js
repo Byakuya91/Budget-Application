a240b167ec9d2a3f9309b2137a927c8012c6335e
Byakuya91 <nganti91@yahoo.com>
2023-07-11 21:12:03 -0400

    Also I have modified the helper function to display color

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,29 @@
-// Local Sotorage
+// color generator function
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+  // HSL:  34 = H,  64 = S, 50 = L
+  return `${existingBudgetLength * 34} 65% 50%`;
+};
+
+// Local Storage
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
+};
+
+// create budget
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    color: generateRandomColor(),
+  };
+  const existingBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
 };
 
 // delete item
